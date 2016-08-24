9be31308385bfd21a12c0e7f45cee8320d70a282
john <jriley555@yahoo.com>
2016-08-24 15:26:48 -0500

    'ready'

---
+++ dist/app/books/books.component.spec.js
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+"use strict";
+var books_component_1 = require('./books.component');
+describe('Component: Books', function () {
+    it('should create an instance', function () {
+        var component = new books_component_1.BooksComponent();
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=books.component.spec.js.map
